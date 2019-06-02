8c9b749f7a156fef7de1c46f7ee0a34ded57bd1c
tantan1984 <tantan198409@gmail.com>
2019-06-02 18:31:22 +0900

    画像を正しく送信できるようにした

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,9 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
+
+  process resize_to_fit: [200, 200]
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -45,7 +47,4 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   "something.jpg" if original_filename
   # end
 
-  include CarrierWave::MiniMagick
-
-  process resize_to_fit: [800, 800]
 end
