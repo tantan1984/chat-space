62b3c9d089fd597f7288db53ac991a6d5353300e
tantan1984 <tantan198409@gmail.com>
2019-06-06 20:35:47 +0900

    画像投稿　被っていたvar htmlを削除

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -17,26 +17,8 @@ function buildHTML(message){
         <asset_path src=${message.image} >
       </div>`
     return html;
-  
-    var html =
-     `<div class="message" data-message-id=${message.id}>
-        <div class="upper-message">
-          <div class="upper-message__user-name">
-            ${message.user_name}
-          </div>
-          <div class="upper-message__date">
-            ${message.date}
-          </div>
-        </div>
-        <div class="lower-message">
-          <p class="lower-message__content">
-            ${message.content}
-          </p>
-        </div>
-      </div>`
-    return html;
   };
-  
+
 $('.js-form').on('submit', function(){
 e.preventDefault();
 var formData = new FormData(this);
