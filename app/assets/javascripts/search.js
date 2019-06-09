eed8e8c018845ae14d426edb7443bce57a187b85
tantan1984 <tantan198409@gmail.com>
2019-06-09 14:25:43 +0900

    グループ・メンバー名の表示の設定

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -12,6 +12,17 @@ $(function(){
     search_list.append(html);
   }
 
+  function appendMembers(name, user_id) {
+    var html = 
+    `<div class='chat-group-user clearfix js-chat-member' id='chat_group_user_22'>
+      <input name='group[user_ids][]' type='hidden' value="${user_id}">
+      <p class='chat-group-user__name'>${name}</p>
+      <a class='user_search_remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+      </div>`
+    member_list.append(html);
+  }
+
+
   function appendErrMsgToHTML(msg) {
     var html = `
                 <div class="chat-group-user clearfix">
@@ -47,8 +58,17 @@ $(function(){
     .fail(function(){
       alert('ユーザー検索に失敗しました');
     })
-
   });
-    
+});
 
+$(function() {
+  $(document).on("click", '.user-search-add', function() {
+    var name = $(this).attr("data-user-name");
+    var user_id = $(this).attr("data-user-id");
+    $(this).parent().remove();
+    appendMembers(name, user_id);
+  });
+  $(document).on("click", '.user-search-remove', function() {
+    $(this).parent().remove();
+  });
 });
